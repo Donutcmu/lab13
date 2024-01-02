c6779ab9ae473b13657b4c8e50a155d8c9c294af
Donut <punnatat_ng@cmu.ac.th>
2024-01-02 16:57:45 +0700

    GG

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool hell[N][M]){
+    cout<<"------------------------------------------------------------------------"<<endl;
+    
+    for(int i=0;i<N;i++){
+        cout<<"|";
+        for(int j=0;j<M;j++){
+            if(hell[i][j]==1){
+                cout<<"*";
+                }
+            else{
+                cout<<" ";
+                }
+        }
+        cout<<"|";
+        cout<<endl;
+    }
+    cout<<"------------------------------------------------------------------------";
+    cout<<endl;
+
+}
+        
+void updateImage(bool hell[N][M],int s,int x,int y){
+        for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            double result;
+            result = sqrt(pow(i-x,2)+pow(j-y,2));
+            if(result<=s-1){
+                hell[i][j]=1;
+                }
+    }
+    }
+    }
